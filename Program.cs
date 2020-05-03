e67da1914a11bac99ba9132625ed0eca41702651
Matilda578 <101889190@student.swin.edu.au>
2020-05-03 13:57:33 +1000

    task1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Hello");
+            }
+            {
+                Console.WriteLine("Welcome Friends!");
+                Console.WriteLine("Have a nice day!");
+            }
+        }
+     }
+ }
